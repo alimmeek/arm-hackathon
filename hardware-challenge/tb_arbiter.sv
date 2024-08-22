3e4d21b973c1131890e66da57fa526bcd1240c3a
Alia Meek <alia.meek@arm.com>
2024-08-22 16:39:50 +0100

    Guys I accidently deleted the testbench

--- hardware-challenge/tb_arbiter.sv
+++ hardware-challenge/tb_arbiter.sv
@@ -1,4 +1,24 @@
-// Cycle through each possible input
+module tb_arbiter();
+
+    logic clk;
+    logic resetn;
+
+    logic [2:0] r;
+    logic [2:0] g;
+
+    // Initialise clock
+    initial begin
+        clk = 1'b0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        resetn = 1'b0;
+        #100
+        resetn = 1'b1;
+    end
+
+    // Cycle through each possible input
     // Do in ascending order of priority
     // Also checks illegal transitions
     // There's a better way to do this probably
@@ -46,7 +66,17 @@
             r = 3'b011;
             #25
             r = 3'b101;
-            #25
+	    #25
             $stop();
         end
     end
+
+    arbiter u_arbiter (
+        .clk(clk),
+        .resetn(resetn),
+        .r(r),
+        .g(g)
+    );
+
+
+endmodule
