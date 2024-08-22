9a221f2f9729c6042cb8b942f404456b1d8a7a95
to_matih <maatteeoh@wp.pl>
2024-08-22 14:32:49 +0100

    better readability

--- directory_reconstruction/src/main.rs
+++ directory_reconstruction/src/main.rs
@@ -4,20 +4,40 @@ use std::io;
 
 const MAX_PATH_LEN: usize = 300;
 
+struct StackString{
+    data: [u8; MAX_PATH_LEN],
+    pos: usize
+}
+
+impl StackString{
+    fn new() -> Self{
+        Self{pos:0, data: [0;MAX_PATH_LEN]}
+    }
+
+    fn append(&mut self, data: &[u8], size: usize){
+        self.data[self.pos .. self.pos+size].copy_from_slice(data);
+        self.pos += size;
+    }
+}
+
 fn main() {
-    // output counters
+    // file total output
     let mut file_total: u32 = 0;
+
+    // average depth output
     let mut current_depth: u32 = 0;
     let mut depth_acc: u32 = 0;
-    let mut current_path: [u8; MAX_PATH_LEN] = [0; 300];
-    current_path[0] = b'/';
-    let mut current_path_pos: usize = 0;
+
+    // deepest nested file output
+    let mut current_path = StackString::new();
+    current_path.data[0] = b'/';
     let mut max_depth: u32 = 0;
-    let mut max_path: [u8; MAX_PATH_LEN] = [0; 300];
-    let mut max_path_len: usize = 0;
+    let mut max_path = StackString::new();
     let mut visited = FxHashSet::default();
     let mut should_chck = false;
-    // allocate buffer
+
+
+    // allocate iput buffer
     let mut buffer = String::new();
     buffer.reserve(MAX_PATH_LEN);
     // read in input
@@ -40,36 +60,40 @@ fn main() {
                                     // moved to root
                                     '/' => {
                                         current_depth = 0;
-                                        current_path_pos = 1;
-                                        current_path[0] = b'/';
+                                        current_path.pos = 1;
+                                        current_path.data[0] = b'/';
                                     }
                                     // moved to parent
                                     '.' => {
                                         current_depth -= 1;
-                                        while current_path[current_path_pos] != b'/' {
-                                            current_path_pos -= 1
+                                        while current_path.data[current_path.pos] != b'/' {
+                                            current_path.pos -= 1
                                         }
-                                        current_path_pos += 1;
+                                        current_path.pos += 1;
                                     }
                                     // moved to child
                                     _ => {
+                                        // increase depth counters
                                         current_depth += 1;
                                         max_depth = std::cmp::max(current_depth, max_depth);
+
+                                        // append directory to current path
                                         let len = buffer.len();
                                         let name_len = len - 6;
-                                        current_path[current_path_pos..current_path_pos + name_len]
-                                            .copy_from_slice(&buffer.as_bytes()[5..len - 1]);
-                                        current_path_pos += name_len;
-                                        current_path[current_path_pos] = b'/';
-                                        current_path_pos += 1;
+                                        current_path.append(&buffer.as_bytes()[5..len - 1], name_len);
+                                        // append separator
+                                        current_path.data[current_path.pos] = b'/';
+                                        current_path.pos += 1;
                                     }
                                 }
                             }
                             // ls commanmd
                             'l' => {
-                                current_path[current_path_pos + 1..].fill(0);
-                                should_chck = visited.insert(current_path);
+                                // check if it is a unique listing
+                                current_path.data[current_path.pos + 1..].fill(0);
+                                should_chck = visited.insert(current_path.data);
                             }
+                            // a command different than ls or cd was issued
                             _ => panic!("Unknown Command"),
                         }
                     }
@@ -77,17 +101,20 @@ fn main() {
                     'd' => {}
                     // ls output file
                     _ => {
+                        // skip non unique listings
                         if should_chck {
+                            // increment file counters
                             file_total += 1;
                             depth_acc += current_depth;
+                            // chech if this is most nested file so far
                             if current_depth == max_depth {
+                                // find name of file
                                 let start_i = buffer.find(' ').unwrap() + 1;
                                 let name_size = buffer.len() - 1 - start_i;
-                                max_path[..current_path_pos]
-                                    .copy_from_slice(&current_path[..current_path_pos]);
-                                max_path[current_path_pos..current_path_pos + name_size]
-                                    .copy_from_slice(&buffer.as_bytes()[start_i..buffer.len() - 1]);
-                                max_path_len = current_path_pos + name_size;
+                                // copy current ffiel path
+                                max_path.pos = 0;
+                                max_path.append(&current_path.data[..current_path.pos], current_path.pos);
+                                max_path.append(&buffer.as_bytes()[start_i..buffer.len() - 1], name_size);
                             }
                         }
                     }
@@ -103,7 +130,7 @@ fn main() {
     println!(
         "{},{},{}",
         file_total,
-        std::str::from_utf8(&max_path[..max_path_len]).unwrap(),
+        std::str::from_utf8(&max_path.data[..max_path.pos]).unwrap(),
         depth_acc as f32 / file_total as f32
     );
 }
